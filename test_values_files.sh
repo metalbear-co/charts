3dd6987f86a64dec22a08d8296658780bf2f5597
Razz4780 <msmolarekg@gmail.com>
2024-12-18 15:49:50 +0100

    Install for real

--- test_values_files.sh
+++ test_values_files.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # for each file in the test_values directory
-# run helm install --dry-run=server
+# run helm install && helm uninstall
 for file in test_values/*.yaml; do
   echo "Running test for $file"
-  helm install --dry-run=server --debug -f $file mirrord-operator ./mirrord-operator
+  helm install --atomic --debug -f $file mirrord-operator ./mirrord-operator --wait
+  helm uninstall mirrord-operator --wait
 done
