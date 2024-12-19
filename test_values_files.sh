5b26079262ebc88a442067c34007afe201bd3189
Razz4780 <msmolarekg@gmail.com>
2024-12-19 14:20:57 +0100

    Grouping logs

--- test_values_files.sh
+++ test_values_files.sh
@@ -5,7 +5,8 @@ set -e
 # for each file in the test_values directory
 # run helm install && helm uninstall
 for file in test_values/*.yaml; do
-  echo "Running test for $file"
+  echo "::group::Running test for $file"
   helm install --atomic --debug -f $file mirrord-operator ./mirrord-operator --wait
   helm uninstall mirrord-operator --wait
+  echo "::endgroup::"
 done
