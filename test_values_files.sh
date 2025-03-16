349c067ce2a6446bf44a0a3f4a50ef6b30257093
Dmitry Dodzin <d.dodzin@gmail.com>
2025-03-16 18:44:42 +0200

    Ops

--- test_values_files.sh
+++ test_values_files.sh
@@ -4,7 +4,7 @@ set -e
 
 # For each file in the `test_values` directory
 # run helm install && helm uninstall.
-for file in "test_values/$1_*.yaml"; do
+for file in test_values/$1_*.yaml; do
   echo "::group::Running test for $file" # Groups logs in the CI dashboard
   helm install --atomic --debug -f $file "mirrord-$1" "./mirrord-$1" --wait
   helm uninstall "mirrord-$1" --wait
